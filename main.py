b8fab2f435e55b37b91ab3ab5f88644d25d932f9
Josh Mitchell <joshmitchell10@gmail.com>
2020-11-30 00:45:32 -0800

    Navals Blog

--- main.py
+++ main.py
@@ -1,96 +1,70 @@
-# import requests
-# from bs4 import BeautifulSoup
-#
-# soup = BeautifulSoup(open('articles.html','r'), 'html.parser')
-#
-# text = ""
-#
-# def print_font(str):
-#     print(str)
-#
-# for link in soup.find_all('a')[1:]:
-#
-#
-#
-#     print(link.get('href'))
-#
-#     page = requests.get("http://paulgraham.com/" + link.get('href'))
-#     soup1 = BeautifulSoup(page.text, 'html.parser')
-#     soup1.prettify()
-#
-#     table = soup1.find('table', border=0, cellspacing=0, width=435)
-#     font = table.find('font', size=2)
-#     print_font(font)
-#
-#     # for text in all_text:
-#     #     print(text.text)
-#
-#     #text += soup1.get_text()
-#
-#     break
-#
-# #with open('saved_text.txt', 'w') as text_file:
-# #    text_file.write(text)
-# print("DONE!!!")
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 import requests
 from bs4 import BeautifulSoup
 import re
 import time
 import random
 
-soup = BeautifulSoup(open('articles.html','r'), 'html.parser')
+soup = BeautifulSoup(open('naval.html','r'), 'html.parser')
 
 text = ""
 
-for link in soup.find_all('a')[1:]:
+dates = ['2005', '2006', '2007', '2008', '2009', '2010', '2011', '2012', '2013', '2014', '2015', '2016', '2017', '2018', '2019', '2020', '2021', '2022']
+def contains_date(line):
+    for date in dates:
+        if date in line:
+            return True
+    return False
 
+def beautify_and_save(text):
+    print('beautifying...')
+    text = text.replace('.', '. ')
+    text = text.replace('.  ', '. ')
+    print('saving...')
+    with open('saved_text.html', 'w') as text_file:
+        text_file.write(text)
 
+for link in soup.find_all('a')[13:-19]:
 
     #print(link.get('href'))
 
-    page = requests.get("http://paulgraham.com/" + link.get('href'))
+    page = requests.get(link.get('href'))
     soup1 = BeautifulSoup(page.text, 'html.parser')
     soup1.prettify()
 
-    i = 0
-
     soup_text = soup1.text
     soup_text.replace('\n', ' ')
-
-    soup_text = soup_text.replace('Want to start a startup?', ' ', 1)
-    soup_text = soup_text.replace('Get funded by', ' ', 1)
-    soup_text = soup_text.replace('Y Combinator.', ' ', 1)
+    #print(soup_text)
 
 
-    text += '<h2 class="chapter">'
-    for word in soup_text.split():
-        #print(word)
-        if i < 30 and word[0:3].isdigit():
-            text += word[:4] + "</h2> \n\n" + word[4:] + ' '
-        else:
-            text += word + " "
-        i += 1
+
+    date_passed = False
+    date_was_last_line = False
+    for line in soup_text.splitlines():
+        line = line.strip('\n')
+
+        if "Subscribe to Naval" in line:
+            break
+
+        if date_was_last_line == True and len(line) != 0:
+            text += '<h2 class="chapter">' + line + '</h2>' + '\n\n\n\n'
+            date_was_last_line = False
+            continue
+
+        if date_passed == False and contains_date(line) == True:
+            date_passed = True
+            date_was_last_line = True
+            text
+        if date_passed == False:
+            continue
+
+        text += line
 
     text += '\n\n\n\n\n\n\n\n\n\n\n'
     print('one done...')
 
-    # if random.randint(0,5) == 5:
-    #      break
+    #if random.randint(0,50) == 5:
+    #break
 
-with open('saved_text.html', 'w') as text_file:
-    text_file.write(text)
+beautify_and_save(text)
+
 print("DONE!!!")
